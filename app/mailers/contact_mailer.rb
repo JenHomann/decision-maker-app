d0d189fa61ef2d392a070412b5e5c5fc958490b7
slamflipstrom <lindstrom.s@gmail.com>
2014-10-14 09:21:04 -0500

    Updated @route instance variable

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,8 +2,7 @@ class ContactMailer < ActionMailer::Base
   default :from => ENV['EMAIL']
   
   def send_vote(contact)
-    #TODO - Test to ensure that round attributes are being passed into mailer send_vote view
-    @round = round
+    @round = contact.round
     @contact = contact
     mail(:to => "#{contact.name} <#{contact.email}>", subject: => "Grupl: Cast your vote!")
   end
