10f61a3823886e84ecf96e361c4314f4a090de58
“Jen <mpjeno@gmail.com>
2014-10-14 11:26:28 -0500

    added round_id to contacts, rounds ANAF contacts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
 
   def home
+    @contact = Contact.new
+    @round = Round.new
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141013205753) do
+ActiveRecord::Schema.define(:version => 20141014162442) do
 
   create_table "contacts", :force => true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20141013205753) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.boolean  "voted"
+    t.integer  "round_id"
   end
 
   create_table "options", :force => true do |t|
