9b47db6fd6bd8d6aef57d853f84160b7aada1790
“Jen <mpjeno@gmail.com>
2014-10-14 11:10:02 -0500

    added random hex to rounds

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,11 @@
 class Contact < ActiveRecord::Base
   attr_accessible :email, :encrypted_id, :name, :phone, :voted
   
-  belongs_to :rounds
-  has_many :votes
-  
   require 'securerandom'
   
   # Sets the encrypted_id
   def set_encrypted_id
     self.encrypted_id = SecureRandom.hex
   end
+
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,7 +1,14 @@
 class Round < ActiveRecord::Base
   attr_accessible :decision, :encrypted_url, :location, :name, :price_range, :restaurant_type
   
+  require 'securerandom'
+  
   has_many :contacts
   has_many :options
+  
+  # Sets the encrypted_url
+  def set_encrypted_url
+    self.encrypted_url = SecureRandom.hex
+  end
   
 end
