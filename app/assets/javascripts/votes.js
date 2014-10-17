adf3913a5dbc94f980d132efa28c8932da0dbf69
slamflipstrom <lindstrom.s@gmail.com>
2014-10-17 10:59:54 -0500

    Fixed hover effect on logo bg in nav bar

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,18 +1,23 @@
 $(document).ready(function(){
  
-  $(".choice").each(function(e){
-    $(".choice").on("click", function(){
+  $(".choice").on("click", function(e){
       $choice = $(this);
       $choice_text = $choice.text(); // Either "Yes" or "No".
       
-      $its_corresponding_option_div = $(".option");
+      $its_corresponding_option_div = $(".button radius");
       
       $its_corresponding_hidden_field = $("#contact_votes_attributes_0_response");
       
       $its_corresponding_hidden_field.val($choice_text);
       
       $its_corresponding_option_div.hide();
-    })
-  });
+    });
 
 });
+
+
+// $( "span" ).click(function() {
+//   $( "li" ).each(function() {
+//     $( this ).toggleClass( "example" );
+//   });
+// });
